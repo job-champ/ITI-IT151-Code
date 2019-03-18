f39c49379dfb66974bfefccc778e791b727fa2d7
Job Champagne <jcham49@lsu.edu>
2019-03-18 02:44:45 -0500

    Search algorithm win form application

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace SearchForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SearchText
+            // 
+            this.SearchText.Location = new System.Drawing.Point(241, 160);
+            this.SearchText.Name = "SearchText";
+            this.SearchText.Size = new System.Drawing.Size(100, 20);
+            this.SearchText.TabIndex = 0;
+            this.SearchText.TextChanged += new System.EventHandler(this.SearchText_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please enter a number to search for:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(241, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(54, 54);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(240, 31);
+            this.Title.TabIndex = 3;
+            this.Title.Text = "Search Application";
+            this.Title.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 230);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SearchText);
+            this.Name = "Form1";
+            this.Text = "SearchApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SearchText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Title;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SearchForm
+{
+    public partial class Form1 : Form
+    {
+        // Class variables for generating random numbers
+
+        private static readonly Random random = new Random();
+        private static readonly object syncLock = new object();
+
+        private static int[] arr = new int[100];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadArray(arr);
+
+            DisplayArray(arr);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SearchText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int searchNumber;
+
+            //int[] arr = new int[100];
+
+            //loadArray(arr);
+
+            //DisplayArray(arr);
+
+            bool isInputNumeric = Int32.TryParse(SearchText.Text, out searchNumber);
+
+            if (isInputNumeric == false)
+                MessageBox.Show("Please enter a numeric value.");
+            else
+            {
+                //loadArray(arr);
+
+                //DisplayArray(arr);
+
+                if (SeqSearch(arr, searchNumber) == true)
+                    MessageBox.Show("Sequential Search found " + searchNumber + " in the array!");
+                else
+                    MessageBox.Show("Sequential Search did not find " + searchNumber + " in the array.");
+            }
+
+        }
+
+        public static bool SeqSearch(int[] arr, int sValue)
+        {
+            for (int i = 0; i < arr.Length; i++)
+
+                if (arr[i] == sValue)
+
+                    return true;
+
+            return false;
+        }
+
+        // This method loads up the array with random integers
+
+        public static void loadArray(int[] arr)
+        {
+            int Min = 0;
+
+            int Max = arr.Length * 10;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                int next = 0;
+
+                while (true)
+                {
+                    next = RandomNumber(Min, Max);
+
+                    if (!Contains(arr, next))
+
+                        break;
+                }
+
+                arr[i] = next;
+            }
+
+        }
+
+        //Function to get a random number 
+
+        public static int RandomNumber(int min, int max)
+        {
+            lock (syncLock)
+            { // synchronize
+
+                return random.Next(min, max);
+            }
+        }
+
+        // Using this to get unique numbers into the array
+
+        public static bool Contains(int[] array, int val)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == val)
+                    return true;
+            }
+
+            return false;
+
+        }
+
+        public static void DisplayArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+
+            Console.WriteLine("");
+        }
+    }
+}
