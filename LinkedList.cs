c31c2491f5fa59e9926f01ee882b30869c036aff
Job Champagne <jcham49@lsu.edu>
2019-04-16 00:21:42 -0500

    Add linked list code

---
+++ LinkedList.cs
@@ -0,0 +1,79 @@
+using System;
+
+public class Node 
+{
+	public Object Element;
+	public Node Link;
+
+	public Node() 
+	{
+		Element = null;
+		Link = null;
+	}
+
+	public Node(Object theElement)
+	{
+		Element = theElement;
+		Link = null;
+	}
+}
+
+public class LinkedList 
+{
+	protected Node header;
+
+	public LinkedList() 
+	{
+		header = new Node("header");
+	}
+
+	private Node Find(Object item) 
+	{
+		Node current = new Node();
+		current = header;
+		while(current != item)
+			current = current.Link;
+		return current;
+	}
+
+	public void Insert(Object newItem, Object after) 
+	{
+		Node current = new Node();
+		Node newNode = new Node(newItem);
+		current = Find(after);
+		newNode.Link = current.Link;
+		current.Link = newNode;
+	}
+
+	private Node FindPrevious(Object n) 
+	{
+		Node current = header;
+		while(!(current.Link == null) && (current.Link.Element != n))
+			current = current.Link;
+		return current;
+	}
+
+	public void Remove(Object n)
+	{
+		Node p = FindPrevious(n);
+		if (!(p.Link == null))
+			p.Link = p.Link.Link;
+	}
+
+	public void PrintList() 
+	{
+		Node current = new Node();
+		current = header;
+		while (!(current.Link == null)) 
+		{
+			Console.WriteLine(current.Link.Element);
+			current = current.Link;
+		}
+	}
+
+	public static void Main()
+	{
+		Console.ReadKey();
+	}
+
+}
